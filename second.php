3a8a8304772d77a9bd9ed1936902a4223357c3f2
procir <tawatokyo@gmail.com>
2020-12-31 15:12:43 +0900

    [ここに変更点

---
+++ second.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Lesson 1</title>
+</head>
+<body>
+<?php echo '入力値：' . $_GET['name']; ?>
+</body>
+</html>
---
+++ third.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>Lesson 1</title>
+</head>
+<body>
+<form action="display.php" method="GET">
+<input type="text" name="name">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
