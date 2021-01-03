57990cc6ab0d0fc834f0ffd69003b8ea70e76878
procir <tawatokyo@gmail.com>
2021-01-03 09:58:45 +0900

    １を削除

--- second.php
+++ second.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>練習</title>
+<title>practice</title>
 </head>
 <body>
 <?php echo '入力値：' . $_GET['name']; ?>
--- third.php
+++ third.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
 <meta charset="UTF-8">
-<title>Lesson</title>
+<title>lesson</title>
 </head>
 <body>
 <form action="display.php" method="GET">
